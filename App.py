188eb57ce39112870a82152577e3ed5bade8fd5e
Ankit kumar <as8818723@gmail.com>
2023-05-03 20:17:38 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,9 +4,7 @@ import pandas as pd
 import numpy as np
 import xgboost
 from xgboost import XGBRegressor
-
 pipe = pickle.load(open('pipe.pkl','rb'))
-
 teams = ['Australia',
  'India',
  'Bangladesh',
@@ -56,7 +54,7 @@ cities = ['Colombo',
 
 st.title('Cricket Score Predictor')
 
-col1, col2 = st.columns(2)
+col1, col2 = st.beta_columns(2)
 
 with col1:
     batting_team = st.selectbox('Select batting team',sorted(teams))
@@ -65,7 +63,7 @@ with col2:
 
 city = st.selectbox('Select city',sorted(cities))
 
-col3,col4,col5 = st.columns(3)
+col3,col4,col5 = st.beta_columns(3)
 
 with col3:
     current_score = st.number_input('Current Score')
@@ -78,13 +76,11 @@ last_five = st.number_input('Runs scored in last 5 overs')
 
 if st.button('Predict Score'):
     balls_left = 120 - (overs*6)
-    wicket_left = 10 - wickets
+    wickets_left = 10 -wickets
     crr = current_score/overs
 
     input_df = pd.DataFrame(
-        {'batting_team': [batting_team], 'bowling_team': [bowling_team], 'city':city, 'current_score': [current_score],
-         'balls_left': [balls_left], 'wickets_left': [wickets], 'crr': [crr], 'last_five': [last_five]})
-
+     {'batting_team': [batting_team], 'bowling_team': [bowling_team],'city':city, 'current_score': [current_score],'balls_left': [balls_left], 'wickets_left': [wickets], 'crr': [crr], 'last_five': [last_five]})
     result = pipe.predict(input_df)
-    st.header("Predicted Score: " + str(int(result[0])))
-    # st.table(input_df)
+    st.header("Predicted Score - " + str(int(result[0])))
+
